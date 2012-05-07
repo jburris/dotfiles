3ecbe02ce737d779f1aae6fb40b62f9737d6bc06
Justin Burris <justinbburris@gmail.com>
2012-05-06 21:15:43 -0400

    removing coffee-script homebrew package

--- pc-setup.rb
+++ pc-setup.rb
@@ -14,7 +14,7 @@ puts "Symlinking dotfiles"
 files.each { |file|  system "ln -s #{dot_folder}/dotfiles/#{file} #{home}/#{file}" }
 
 # Install system packages with brew
-brew_packages = %w{ack bazaar coffee-script git imagemagick memcached mongodb mysql node rbenv rbenv-gemset wget}
+brew_packages = %w{ack bazaar git imagemagick memcached mongodb mysql node rbenv rbenv-gemset wget}
 
 puts "Installing homebrew packages"
 brew_packages.each do |package|
