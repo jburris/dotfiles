8121e42b5ccc75c83807149f55396789c8935d04
Justin Burris <justinbburris@gmail.com>
2012-05-06 21:30:39 -0400

    Shell now knows to call ruby for script execution Some basic autolaunching code for memcached

--- pc-setup.rb
+++ pc-setup.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # Assumes the following programs have been installed and configured: homebrew & git
 # Also assumes you've checked out the dotfiles repository :)
 
@@ -21,3 +22,11 @@ brew_packages.each do |package|
   puts "Installing : #{package}"
   system "brew install #{package}" 
 end
+
+###### Package specific configurations (auto-launch, user creation, etc)
+
+system "mkdir -p ~/Library/LaunchAgents"
+
+#memcached
+system "cp /usr/local/Cellar/memcached/1.4.13/homebrew.mxcl.memcached.plist ~/Library/LaunchAgents/"
+system "launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist"
