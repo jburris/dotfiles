e697e68e2596e547e6cef0ba6ead8be85d3057dd
Justin Burris <justinbburris@gmail.com>
2012-12-01 08:31:50 -0500

    remap alt/option to meta & cmd to super

--- dotfiles/.emacs.el
+++ dotfiles/.emacs.el
@@ -6,6 +6,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(js-indent-level 2)
+ '(mac-command-modifier (quote super))
+ '(mac-option-modifier (quote meta))
  '(tab-width 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
